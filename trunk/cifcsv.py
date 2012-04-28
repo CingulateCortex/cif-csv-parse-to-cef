8c6e432d70e72189aa6dbb51a9c2fed7550149d1
dfirn00b@gmail.com <dfirn00b@gmail.com@cf2a4878-54a7-2ead-0ff4-f8d5912163e1>
2012-04-28 05:08:27 +0000

    Initial Upload

---
+++ trunk/cifcsv.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+import csv, sys,socket
+from optparse import OptionParser
+
+def parsecsv(file):
+  mtype = options.mtype
+  reader = csv.DictReader(open(file))
+  for row in reader:
+     address = row.get('# address')
+     altid = row.get('alternativeid')
+     confidence = row.get('confidence')
+     description = row.get('description')
+     message= "CEF:0|CIF|CIF 0.1|100|1|CIF Malicious %s|1|shost=%s cs1=%s cs1Label=Source  cs2=%s cs2Label=ConfidenceLevel cs3=%s cs3Label=Description" %(mtype,address,altid,confidence,description)
+     syslog(message, options.host, options.port)
+
+def syslog(message,host,port):
+   syslog = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+   data = '<%d>%s' % (5 + 8*3, message)
+   print data
+   syslog.sendto(data,(host,port))
+   syslog.close()
+
+parser = OptionParser(usage="%prog [-f] [-s]", version="%prog 0.01")
+parser.add_option("-f", action="store",dest="File Name", help="Name of the file you would like to parse")
+parser.add_option("-s", action="store",dest="Host", help="IP address or FQDN of syslog host to send CEF Messages to")
+parser.add_option("-p", action="store", type="int",dest="Port", default=514,help="PORT to send syslog messages to. Usually 514 please check your Connector configuration [DEFAULT: 514]")
+parser.add_option("-t", action="store",dest="Message Type", default="Domain",help="Domain or IP this will help generate the CEF Message that is sent. If Domain the CEF Name will be CIF Malicious Domain. If IP the CEF Name will be CIF Malicious IP")
+(options, args) = parser.parse_args()
+if options.fname == None :
+   parser.error("You must specify a filename ")
+   sys.exit(1)
+elif options.host == None:
+   parser.error("You must specify a host")
+   sys.exit(1)
+else:
+   parsecsv(options.fname)
